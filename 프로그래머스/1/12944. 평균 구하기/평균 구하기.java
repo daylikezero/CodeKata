1c51457f38bf0c7a3e63171a553028ea386a96e9
daylikezero <daylikezero@gmail.com>
2024-12-24 09:33:03 +0900

    [level 1] Title: 평균 구하기, Time: 4.41 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = Arrays.stream(arr).average().getAsDouble();
+        return answer;
+    }
+}
