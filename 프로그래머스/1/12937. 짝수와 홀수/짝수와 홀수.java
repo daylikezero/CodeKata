920effe1c2505f00ac6bbe0837592a7eb8637304
daylikezero <daylikezero@gmail.com>
2024-12-24 09:27:56 +0900

    #1:Update 짝수와 홀수.java
    삼항연산자 사용

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
-        if(num % 2 == 0) {
-            answer = "Even";
-        } else {
-            answer = "Odd";
-        }
+        String answer = num % 2 == 0 ? "Even" : "Odd";
         return answer;
     }
 }
