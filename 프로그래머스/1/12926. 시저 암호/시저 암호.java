256636a7540bbb76ee32e95237f2b352f985b8ee
daylikezero <daylikezero@gmail.com>
2025-01-07 09:35:19 +0900

    [level 1] Title: 시저 암호, Time: 2.24 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,12 +3,15 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i)>= 'a' && s.charAt(i)<= 'z') {
+                // 97('a') ~ 122('z')
                 int shift = (s.charAt(i) - 'a' + n) % 26 + 'a';
                 sb.append(Character.toChars(shift));
             } else if (s.charAt(i)>= 'A' && s.charAt(i)<= 'Z') {
+                // 65('A') ~ 90('Z') 
                 int shift = (s.charAt(i) - 'A' + n) % 26 + 'A';
                 sb.append(Character.toChars(shift));
             } else {
+                // 공백문자
                 sb.append(s.charAt(i));
             }
         }
