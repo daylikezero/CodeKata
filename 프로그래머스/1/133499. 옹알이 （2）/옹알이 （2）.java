8134f32afe808224b313ab7f024dc934b074152a
daylikezero <daylikezero@gmail.com>
2025-01-23 09:20:54 +0900

    [level 1] Title: 옹알이 (2), Time: 9.76 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        List<String> babbles = new ArrayList<>(List.of("aya", "ye", "woo", "ma"));
+        for (String word : babbling) {
+            if (babbles.contains(word)) {
+                answer++;
+            } else {
+                // 조합하여 발음할 수 있는 단어인지 확인
+                if (isPossible(word, babbles)) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public static boolean isPossible(String word, List<String> babbles) {
+        for (String babble : babbles) {
+            if (word.contains(babble + babble)) {
+                return false;
+            }
+            word = word.replaceAll(babble, "0");
+        }
+        return word.replaceAll("0", "").trim().isEmpty();
+    }
+}
